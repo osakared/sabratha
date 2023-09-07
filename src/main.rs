1c01c7343fe67d76fbce27323e43d0ff6dfcd438
Thomas J. Webb <thomas@osakared.com>
2023-09-06 21:23:29 -0700

    Literally just adding stuff to a container

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,11 @@
-use rusqlite::{Connection, Result};
-pub mod model;
-use crate::model::Table;
-use crate::model::Serializable;
+pub mod time_series;
 
-#[derive(Debug)]
-struct Measurement {
-    force: f32,
-    unit: String,
-}
+use chrono::Utc;
+use crate::time_series::Database;
 
-impl Serializable for Measurement {
-    fn serialize(&self) -> String {
-        format!("force: {}{}", self.force, self.unit)
-    }
-}
-
-fn main() -> Result<()> {
-    let conn = Connection::open_in_memory()?;
-
-    let measurement = Measurement {
-        force: 4320.003,
-        unit: "N".to_string(),
-    };
-    println!("Inserting measurement: {:?}", measurement);
-    let table:Table<Measurement> = Table::new(&conn);
-    table.init(&conn)?;
-    table.add_row(measurement, &conn)?;
-    table.show_rows(&conn)?;
-
-    Ok(())
+fn main() {
+    let mut db = Database::new();
+    db.append("101", Utc::now(), 0.123);
+    db.append("101", Utc::now(), 4.23);
+    println!("{:#?}", db);
 }
